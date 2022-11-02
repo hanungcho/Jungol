08a7f5b71df6b0c7cb8d7c0449c5e412d3d469c9
Administrator <Administrator@User-2022TAVVJD>
2022-11-02 14:07:03 +0900

    push head

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -1,10 +1,28 @@
 package test;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("abc2020.10.312");
 
+		Scanner sc = new Scanner(System.in);
+
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+
+		sc.close();
+
+		int sum = num1 + num2 + num3;
+		int avg = sum / 3;
+
+		System.out.println("sum :" + sum);
+		System.out.println("agv :" + avg);
+//소수점 자리까지 표현하겠다 하면 
+//		dobule agv = (dobyle) sum / 3; 
+//		System.out.printl(avg : %.3f\n, avg); //소수점 3자리까지 표현하겠다 
+		// 여기서 .3은 3자리까지  표현하겠다 f는 실수 슬러시엔까지 써준다
 	}
 
 }
