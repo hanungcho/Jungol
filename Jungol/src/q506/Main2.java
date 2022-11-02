6a4d9cf2de109d4cd15b43859328c596c7bf7d5c
Administrator <Administrator@User-2022TAVVJD>
2022-11-02 16:21:57 +0900

    정올 입력 자가진단2 #510

---
+++ Jungol/src/q506/Main2.java
@@ -0,0 +1,21 @@
+package q506;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+
+		int a = 0;
+		int b = 1;
+
+		int z = -1;
+		int x = 100;
+
+		int result1 = a + z;
+		int result2 = b * x;
+
+		System.out.println(result1);
+		System.out.println(result2);
+
+	}
+
+}
