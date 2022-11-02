6d3ef6dc8aa4000a0d2f2f515bd782a7f9237909
Administrator <Administrator@User-2022TAVVJD>
2022-11-02 13:58:48 +0900

    출력6

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,21 @@
+package q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// System.out.println("MY height");
+//		System.out.println("170");
+//		System.out.println("MY weight");
+//		System.out.println("68.600000");
+		int height = 170;
+		double weight = 68.60000;
+
+		System.out.printf("MY height\n");
+		System.out.printf("%d\n", height);
+		System.out.printf("My weight\n");
+		System.out.printf("%6f", weight);
+
+	}
+
+}
